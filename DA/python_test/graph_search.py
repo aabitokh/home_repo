dd62c8932eafed217284f2ccb5484da1a1683a4e
aabitokh <aabitokh@gmail.com>
2022-12-03 20:44:41 +0100

    grapgh search update

--- DA/python_test/graph_search.py
+++ DA/python_test/graph_search.py
@@ -26,12 +26,40 @@ costs['b'] = 2
 costs['fin'] = infinity
 
 
+def find_lowes_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
 parents = {}
 parents['a'] = 'start'
 parents['b'] = 'start'
 parents['fin'] = None
 
 processed = []
+
+node  = find_lowes_cost_node(costs)
+while node is not None: 
+    cost = costs[node]
+    neighbors = graph[node]
+
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowes_cost_node(costs)
+
+
+
 
 
 
